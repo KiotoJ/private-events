6ea8ed0cc28cf0f73cceac0fc9976abc729fe6ec
Jaona <jrasolomanana@ingenosya.mg>
2024-05-19 18:40:21 +0300

    (q.4) Add foreign_key user_id to event and adapt event model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = Event.build
+  end
+
+  it { expect(described_class.reflect_on_association(:creator).macro).to eq(:belongs_to) }
 end
