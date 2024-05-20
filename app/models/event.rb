4b26cec70e76ac0ce488d176ef1db46153f4689e
Jaona <jrasolomanana@ingenosya.mg>
2024-05-20 17:41:48 +0300

    (q.final_touches.1) Add Event.past Event.future and test

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, foreign_key: :user_id, class_name: 'User'
   has_many :event_attendees, foreign_key: :event_id, inverse_of: :attended_event
   has_many :attendees, through: :event_attendees, class_name: "User", source: :attendee
+
+  def self.past
+    where('event_date <= ?', Date.today)
+  end
+
+  def self.future
+    where('event_date > ?', Date.today)
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe Event, type: :model do
 
   it { expect(described_class.reflect_on_association(:creator).macro).to eq(:belongs_to) }
   it { expect(described_class.reflect_on_association(:attendees).macro).to eq(:has_many) }
+
+  describe "Event.past" do
+    before do
+      user_1 = User.create(email: "fro@mi.mi", password: "reop11111")
+      user_2 = User.create(email: "secon@mi.mi", password: "11111111")
+      @past_event = Event.create(creator: user_1, attendees: [user_1, user_2], event_date: "2002-3-21")
+      @future_event = Event.create(creator: user_1, attendees: [user_1, user_2], event_date: "2032-3-21")
+    end
+
+    it "returns past" do
+      expect(Event.past).to eq([@past_event])
+    end
+  end
+
+  describe "Event.future" do
+    before do
+      user_1 = User.create(email: "fro@mi.mi", password: "reop11111")
+      user_2 = User.create(email: "secon@mi.mi", password: "11111111")
+      @past_event = Event.create(creator: user_1, attendees: [user_1, user_2], event_date: "2002-3-21")
+      @future_event = Event.create(creator: user_1, attendees: [user_1, user_2], event_date: "2032-3-21")
+    end
+
+    it "returns past" do
+      expect(Event.future).to eq([@future_event])
+    end
+  end
 end
