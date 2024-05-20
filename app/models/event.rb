909725bf939039f6d12d4d788232903c8b6fa547
Jaona <jrasolomanana@ingenosya.mg>
2024-05-20 17:44:59 +0300

    (q.final_touches.2) Refractor the past and future by using scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,6 @@ class Event < ApplicationRecord
   has_many :event_attendees, foreign_key: :event_id, inverse_of: :attended_event
   has_many :attendees, through: :event_attendees, class_name: "User", source: :attendee
 
-  def self.past
-    where('event_date <= ?', Date.today)
-  end
-
-  def self.future
-    where('event_date > ?', Date.today)
-  end
+  scope :past, -> { where('event_date <= ?', Date.today) }
+  scope :future, -> { where('event_date > ?', Date.today) }
 end
